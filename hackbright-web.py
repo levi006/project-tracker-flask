f2f05553d498ad72036a767ce81c8eaebb660532
Hackbright Student <info@hackbrightacademy.com>
2015-04-28 18:05:58 -0700

    completed step one of project exercise. form working.

--- hackbright-web.py
+++ hackbright-web.py
@@ -8,9 +8,21 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    html = render_template("student_info.html",
+                            first=first,
+                            last=last,
+                            github=github)
+
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
